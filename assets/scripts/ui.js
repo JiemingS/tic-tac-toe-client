c51cfc26b9ce48c59bc4aa6572b13ca2c7f3ed25
JiemingS <sunjieming1121@gmail.com>
2019-10-01 16:41:33 -0400

    add some clean function

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -19,6 +19,7 @@ const failureMessage = function (newText) {
 const onSignUpSuccess = function () {
   successMessage('Signed up successfully!')
   setTimeout(function () { $('#message').text('') }, 1000)
+  $('#sign-up').trigger('reset')
 }
 
 const onSignUpFailure = function () {
@@ -50,6 +51,7 @@ const onSignInFailure = function () {
 const onChangePasswordSuccess = function () {
   successMessage('Changed password successfully!')
   setTimeout(function () { $('#message').text('') }, 1000)
+  $('#change-password').trigger('reset')
 }
 
 const onChangePasswordFailure = function () {
