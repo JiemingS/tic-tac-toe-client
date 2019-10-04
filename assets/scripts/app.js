e3e47941e6de368c5b7f1db881dd30ee6b19fdd2
JiemingS <sunjieming1121@gmail.com>
2019-10-03 21:26:20 -0400

    add some logic to the AI

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -35,9 +35,23 @@ $(() => {
     events.onCreateGame()
     addClickFunction()
   })
+  // the button for against AI
+  $('#new-game-vs-computer').click(function () {
+    cleanForNewGame()
+    step = 0
+    events.onCreateGame()
+    addClickFunctionAI()
+  })
+  $('#new-game-vs-computer-2').click(function () {
+    cleanForNewGame()
+    step = 0
+    events.onCreateGame()
+    addClickFunctionAILv2()
+  })
   // --------------------------------------------------------------------------
   const emptyJsBoard = ['', '', '', '', '', '', '', '', '']
   let step = 0
+  let someoneWin = 0
   // --------------------------------------------------------------------------
   // to fill the blank with X or O
   // const updateData = {
@@ -78,6 +92,7 @@ $(() => {
       $('#block' + (i + 1)).text('')
     }
     step = -1
+    someoneWin = 0
   }
 
   // ---------------------------------------------------------------------------
@@ -91,6 +106,7 @@ $(() => {
           events.onUpdate('', '', true)
           events.onIndex()
           $('#main-message').text('Player ' + emptyJsBoard[0] + ' Win')
+          someoneWin = 1
           clean()
           break
         }
@@ -100,6 +116,7 @@ $(() => {
           events.onUpdate('', '', true)
           events.onIndex()
           $('#main-message').text('Player ' + emptyJsBoard[6] + ' Win')
+          someoneWin = 1
           clean()
           break
         }
@@ -109,6 +126,7 @@ $(() => {
           events.onUpdate('', '', true)
           events.onIndex()
           $('#main-message').text('Player ' + emptyJsBoard[rank] + ' Win')
+          someoneWin = 1
           clean()
           break
         }
@@ -118,6 +136,7 @@ $(() => {
           events.onUpdate('', '', true)
           events.onIndex()
           $('#main-message').text('Player ' + emptyJsBoard[rank] + ' Win')
+          someoneWin = 1
           clean()
           break
         }
@@ -129,6 +148,92 @@ $(() => {
         clean()
         break
       }
+    }
+  }
+
+  const AIMoveLv1 = function () {
+    checkWin()
+    if (someoneWin === 0 && step !== 9) {
+      const indexs = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+      const result = indexs.filter(index => emptyJsBoard[index] === '')
+      // console.log(result)
+      const rand = result[Math.floor(Math.random() * result.length)]
+      // console.log(rand)
+      // events.onUpdate(rand, 'O', '')
+      emptyJsBoard[rand] = 'O'
+      // console.log(emptyJsBoard)
+      $('#block' + (rand + 1)).text('O')
+      // -----------------------------------------------------------------------
+      $('#main-message').text('Round: Player1')
+      checkWin()
+      step++
+    }
+  }
+
+  const winWaysArray = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 6], [0, 4, 8]]
+
+  const choiceAI = function () {
+    for (let i = 0; i < 8; i++) {
+      if (emptyJsBoard[winWaysArray[i][0]] === emptyJsBoard[winWaysArray[i][1]] && emptyJsBoard[winWaysArray[i][0]] === 'O' && emptyJsBoard[winWaysArray[i][2]] === '') {
+        return winWaysArray[i][2]
+      } else if (emptyJsBoard[winWaysArray[i][0]] === emptyJsBoard[winWaysArray[i][2]] && emptyJsBoard[winWaysArray[i][0]] === 'O' && emptyJsBoard[winWaysArray[i][1]] === '') {
+        return winWaysArray[i][1]
+      } else if (emptyJsBoard[winWaysArray[i][1]] === emptyJsBoard[winWaysArray[i][2]] && emptyJsBoard[winWaysArray[i][1]] === 'O' && emptyJsBoard[winWaysArray[i][0]] === '') {
+        return winWaysArray[i][0]
+      }
+    }
+    for (let i = 0; i < 8; i++) {
+      if (emptyJsBoard[winWaysArray[i][0]] === emptyJsBoard[winWaysArray[i][1]] && emptyJsBoard[winWaysArray[i][0]] === 'X' && emptyJsBoard[winWaysArray[i][2]] === '') {
+        return winWaysArray[i][2]
+      } else if (emptyJsBoard[winWaysArray[i][0]] === emptyJsBoard[winWaysArray[i][2]] && emptyJsBoard[winWaysArray[i][0]] === 'X' && emptyJsBoard[winWaysArray[i][1]] === '') {
+        return winWaysArray[i][1]
+      } else if (emptyJsBoard[winWaysArray[i][1]] === emptyJsBoard[winWaysArray[i][2]] && emptyJsBoard[winWaysArray[i][1]] === 'X' && emptyJsBoard[winWaysArray[i][0]] === '') {
+        return winWaysArray[i][0]
+      }
+    }
+    // const indexs = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    const indexs = [4]
+    const result = indexs.filter(index => emptyJsBoard[index] === '')
+    if (result.length !== 0) {
+      return result[Math.floor(Math.random() * result.length)]
+    } else {
+      const indexsS = [0, 2, 6, 8]
+      const resultS = indexsS.filter(index => emptyJsBoard[index] === '')
+      if (resultS.length !== 0) {
+        // console.log('result ', result)
+        // console.log('by random')
+        return resultS[Math.floor(Math.random() * resultS.length)]
+      } else {
+        const indexsN = [1, 3, 5, 7]
+        const resultN = indexsN.filter(index => emptyJsBoard[index] === '')
+        // console.log('result ', result)
+        // console.log('by random')
+        return resultN[Math.floor(Math.random() * resultN.length)]
+      }
+    }
+    // console.log('result ', result)
+    // console.log('by random')
+    // return result[Math.floor(Math.random() * result.length)]
+  }
+
+  const AIMoveLv2 = function () {
+    checkWin()
+    // console.log(step)
+    if (someoneWin === 0 && step !== 9) {
+      // const indexs = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+      // const result = indexs.filter(index => emptyJsBoard[index] === '')
+      // console.log('AI ', result)
+      // const choice = result[Math.floor(Math.random() * result.length)]
+      const choice = choiceAI()
+      // console.log('choice ', choice)
+      // events.onUpdate(choice, 'O', '')
+      emptyJsBoard[choice] = 'O'
+      // console.log(emptyJsBoard)
+      $('#block' + (choice + 1)).text('O')
+      // -----------------------------------------------------------------------
+      $('#main-message').text('Round: Player1')
+      checkWin()
+      step++
     }
   }
 
@@ -164,6 +269,98 @@ $(() => {
             $('#main-message').text('Round: Player1')
             checkWin()
             step++
+          }
+        } else {
+          const element = document.querySelector('.box' + blockNum)
+          element.classList.add('animated', 'shake')
+          setTimeout(() => {
+            element.classList.remove('animated', 'shake')
+          }, 1000)
+        }
+      })
+    }
+  }
+
+  const addClickFunctionAI = function () {
+    $('#main-message').text('GAME START ! Play1 "X" First')
+    for (let blockNum = 1; blockNum < 10; blockNum++) {
+      $('#block' + blockNum).on('click', function (event) {
+        if ($('#block' + blockNum).text() === '') {
+          if (step % 2 === 0) {
+            const cellIndex = Number($(event.target).attr('data-index'))
+            // console.log('cellIndex = ', cellIndex)
+            const cellValue = 'x'
+            // console.log('cellValue = ', cellValue)
+            // api.updateGame(cellIndex, cellValue, '')
+            events.onUpdate(cellIndex, cellValue, '')
+            $('#block' + blockNum).text('X')
+            emptyJsBoard[blockNum - 1] = 'X'
+            // events.onUpdate(JSON.stringify(updateData))
+            // console.log(emptyJsBoard + ' step ' + step)
+            $('#main-message').text(' Round: Player2')
+            checkWin()
+            step++
+            AIMoveLv1()
+          } else {
+            const cellIndex = Number($(event.target).attr('data-index'))
+            // console.log('cellIndex = ', cellIndex)
+            const cellValue = 'o'
+            // console.log('cellValue = ', cellValue)
+            // api.updateGame(cellIndex, cellValue, '')
+            events.onUpdate(cellIndex, cellValue, '')
+            $('#block' + blockNum).text('O')
+            emptyJsBoard[blockNum - 1] = 'O'
+            // console.log(emptyJsBoard + ' ' + step)
+            $('#main-message').text('Round: Player1')
+            checkWin()
+            step++
+            AIMoveLv1()
+          }
+        } else {
+          const element = document.querySelector('.box' + blockNum)
+          element.classList.add('animated', 'shake')
+          setTimeout(() => {
+            element.classList.remove('animated', 'shake')
+          }, 1000)
+        }
+      })
+    }
+  }
+
+  const addClickFunctionAILv2 = function () {
+    $('#main-message').text('GAME START ! Play1 "X" First')
+    for (let blockNum = 1; blockNum < 10; blockNum++) {
+      $('#block' + blockNum).on('click', function (event) {
+        if ($('#block' + blockNum).text() === '') {
+          if (step % 2 === 0) {
+            const cellIndex = Number($(event.target).attr('data-index'))
+            // console.log('cellIndex = ', cellIndex)
+            const cellValue = 'x'
+            // console.log('cellValue = ', cellValue)
+            // api.updateGame(cellIndex, cellValue, '')
+            events.onUpdate(cellIndex, cellValue, '')
+            $('#block' + blockNum).text('X')
+            emptyJsBoard[blockNum - 1] = 'X'
+            // events.onUpdate(JSON.stringify(updateData))
+            // console.log(emptyJsBoard + ' step ' + step)
+            $('#main-message').text(' Round: Player2')
+            checkWin()
+            step++
+            AIMoveLv2()
+          } else {
+            const cellIndex = Number($(event.target).attr('data-index'))
+            // console.log('cellIndex = ', cellIndex)
+            const cellValue = 'o'
+            // console.log('cellValue = ', cellValue)
+            // api.updateGame(cellIndex, cellValue, '')
+            events.onUpdate(cellIndex, cellValue, '')
+            $('#block' + blockNum).text('O')
+            emptyJsBoard[blockNum - 1] = 'O'
+            // console.log(emptyJsBoard + ' ' + step)
+            $('#main-message').text('Round: Player1')
+            checkWin()
+            step++
+            AIMoveLv2()
           }
         } else {
           const element = document.querySelector('.box' + blockNum)
